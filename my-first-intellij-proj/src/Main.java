ef559617bfe881367fc4190e7dd22164e89c8e62
Josh Archer <jarcher@greenriver.edu>
2023-01-03 11:05:43 -0800

    Added a CellPhone class.

---
+++ my-first-intellij-proj/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //create an instance of our CellPhone class
+        CellPhone myPhone = new CellPhone("Samsung S22", 3, "USB-C", 128.0);
+
+        System.out.println(myPhone);
+    }
+}
