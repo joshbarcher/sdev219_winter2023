4230f742072b0e83be5c6155cbf043d3465e6b53
Josh Archer <jarcher@greenriver.edu>
2023-02-28 11:00:35 -0800

    Added an example of recursive methods.

---
+++ recursion/src/Main.java
@@ -0,0 +1,83 @@
+public class Main
+{
+    private int methodCount;
+
+    public static void main(String[] args)
+    {
+        Main program = new Main();
+
+        //start "recursing"
+        String[] words = {"Circumlocution", "Percolate", "Extraneous", "Onomatopoeia", "Panegyrize", "Overmorrow"};
+        program.printArray(words);
+    }
+
+    //regular method
+    private void printArray(String[] words)
+    {
+        printArray(words, 0);
+    }
+
+    //recursive method
+    private void printArray(String[] words, int currentIndex)
+    {
+        //base case
+        if (currentIndex < 0 || currentIndex >= words.length)
+        {
+            return; //exit!
+        }
+        else
+        {
+            //print out the array element at the current position
+            System.out.println(words[currentIndex]);
+
+            //recurse and move towards our base case
+            currentIndex++;
+            printArray(words, currentIndex);
+        }
+    }
+
+    //regular method
+    private void blastOff()
+    {
+        blastOff(10);
+    }
+
+    //recursive method
+    private void blastOff(int step)
+    {
+        //base case
+        if (step == 0)
+        {
+            System.out.println("Blast-off!");
+        }
+        else //recursive case
+        {
+            System.out.println(step);
+            blastOff(step - 1);
+        }
+    }
+
+    private void printDown(int num)
+    {
+        //base case
+        if (num == 1)
+        {
+            System.out.println(num);
+        }
+        else
+        {
+            printDown(num - 1);
+            System.out.println(num);
+        }
+    }
+
+    private void doSomething()
+    {
+        methodCount++;
+        System.out.println("Method Call #" + methodCount);
+
+        //this is a recursive call
+        doSomething();
+    }
+}
+
